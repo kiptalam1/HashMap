2541fb5e57f3816520965dbf2d65d925fff73bd2
adams <adamskiptalam0@gmail.com>
2025-01-20 16:49:09 +0300

    Add function to double capacity when size exceeds capacity * loadFactor

--- hashMap.js
+++ hashMap.js
@@ -10,12 +10,29 @@ class HashMap {
 
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+          hashCode = primeNumber * hashCode + key.charCodeAt(i);
         }
 
-        return hashCode;
+        return hashCode % this.capacity;
     } 
+    resize() {
+        const newCapacity = this.capacity * 2;
+        const newBuckets = new Array(newCapacity);
+
+        for (let bucket of this.buckets) {
+            if (bucket) {
+                for (let [key, value] of bucket) {
+                    let newIndex = this.hash(key) % newCapacity;
+                    if (!newBuckets[newIndex]) newBuckets[newIndex] = [];
+                    newBuckets[newIndex].push([key, value]);
+                }
+            }
+        }
+        this.buckets = newBuckets;
+        this.capacity = newCapacity;
+    }
     set(key, value) {
+        if (this.size >= this.capacity * this.loadFactor) this.resize();
         let index = this.hash(key);
 
         if (!this.buckets[index]) {
@@ -23,6 +40,7 @@ class HashMap {
         };
 
         this.buckets[index].push([key, value]);
+        this.size++;
     }
     get(key) {
         let index = this.hash(key);
@@ -31,11 +49,12 @@ class HashMap {
             return null;
         }
 
-        for (let bucket of this.buckets[index]) {
-            if (bucket[0] === key) {
-                return bucket[1];
+        for (let [storedKey, storedValue] of this.buckets[index]) {
+            if (storedKey === key) {
+                return storedValue;
             }
         }
+        return null;
     }
     has(key) {
         let index = this.hash(key);
@@ -54,9 +73,13 @@ class HashMap {
         for (let [i, bucket] of this.buckets[index].entries()) {
             if (bucket[0] === key) {
                 this.buckets[index].splice(i, 1);
+                this.size--;
                 return true;
             }
         }
+    }
+    length() {
+        return this.size;
     }
 }
 
@@ -71,4 +94,6 @@ console.log(myMap.has('life'))
 
 
 
+console.log(myMap.size)
 console.log(myMap.buckets)
+
